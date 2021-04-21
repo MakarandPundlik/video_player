911b5ea57dd39804d8efbb52a842d6c83b2f9bec
MakarandPundlik <mmp2308@gmail.com>
2021-04-21 14:29:48 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
+import { AppBar, Container, makeStyles, Toolbar, Typography } from '@material-ui/core';
+
 import React from 'react';
+import ReactPlayer from 'react-player';
+
+
 function App() {
+  const useStyles = makeStyles({
+    playerWrapper:{
+      width:"100%",
+      position:"relative"
+    },
+    controlsWrapper:{
+      position:"absolute",
+      top:0,
+      bottom:0,
+      left:0,
+      right:0,
+      background:"rgba(0,0,0,0.5)",
+      display:"flex",
+      flexDirection:"column",
+      justifyContent:"space-between",
+      zIndex:1
+    }
+  });
+  
+  const classes = useStyles();
   return (
-    <div className="App">
-     
-    </div>
+    <>
+    <AppBar position="fixed">
+      <Toolbar>
+        <Typography variant="h4">React Video Player</Typography>
+      </Toolbar>
+    </AppBar>
+    <Toolbar/>
+    <Container maxWidth="md">
+      <div className={classes.playerWrapper}>
+      <ReactPlayer url="https://www.youtube.com/watch?v=UfmwwEtjYW8"
+      width="100%"
+      muted={false}
+      playing={true}
+      >
+      </ReactPlayer>
+      
+      <div className={classes.controlsWrapper}>
+      </div>
+      </div>
+    </Container>
+    </>
   );
 }
 
